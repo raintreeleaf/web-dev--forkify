332fa18ee6ae1d45f5614555feb7bfdcbe07b733
Lee Kah Keong <raintreeleaf1@gmail.com>
2023-05-14 16:01:19 +0800

    Modified addRecipeView to remove bug in upload window after displaying messages

--- starter/src/js/views/addRecipeView.js
+++ starter/src/js/views/addRecipeView.js
@@ -19,6 +19,19 @@ class AddRecipeView extends View {
   toggleWindow() {
     this._overlay.classList.toggle('hidden');
     this._window.classList.toggle('hidden');
+    // To render the upload window after the display of success or error messages
+    // timeout is to solve the bug of the window displaying the rendered markup
+    // for a fraction of a second as you close it, until a better method is found.
+    setTimeout(_ => {
+      const windowClass = this._parentElement.firstElementChild.classList[0];
+      if (windowClass === 'error' || windowClass === 'message') {
+        this._clear();
+        this._parentElement.insertAdjacentHTML(
+          'afterbegin',
+          this._generateMarkup()
+        );
+      }
+    }, 500);
   }
 
   _addHandlerShowWindow = function () {
@@ -38,7 +51,77 @@ class AddRecipeView extends View {
       handler(data);
     });
   };
-  _generateMarkup() {}
+  _generateMarkup() {
+    const markup = `<form class="upload">
+    <div class="upload__column">
+      <h3 class="upload__heading">Recipe data</h3>
+      <label>Title</label>
+      <input value="TEST" required name="title" type="text" />
+      <label>URL</label>
+      <input value="TEST" required name="sourceUrl" type="text" />
+      <label>Image URL</label>
+      <input value="TEST" required name="image" type="text" />
+      <label>Publisher</label>
+      <input value="TEST" required name="publisher" type="text" />
+      <label>Prep time</label>
+      <input value="23" required name="cookingTime" type="number" />
+      <label>Servings</label>
+      <input value="23" required name="servings" type="number" />
+    </div>
+
+    <div class="upload__column">
+      <h3 class="upload__heading">Ingredients</h3>
+      <label>Ingredient 1</label>
+      <input
+        value="0.5,kg,Rice"
+        type="text"
+        required
+        name="ingredient-1"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+      <label>Ingredient 2</label>
+      <input
+        value="1,,Avocado"
+        type="text"
+        name="ingredient-2"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+      <label>Ingredient 3</label>
+      <input
+        value=",,salt"
+        type="text"
+        name="ingredient-3"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+      <label>Ingredient 4</label>
+      <input
+        type="text"
+        name="ingredient-4"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+      <label>Ingredient 5</label>
+      <input
+        type="text"
+        name="ingredient-5"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+      <label>Ingredient 6</label>
+      <input
+        type="text"
+        name="ingredient-6"
+        placeholder="Format: 'Quantity,Unit,Description'"
+      />
+    </div>
+
+    <button class="btn upload__btn">
+      <svg>
+        <use href="src/img/icons.svg#icon-upload-cloud"></use>
+      </svg>
+      <span>Upload</span>
+    </button>
+  </form>`;
+    return markup;
+  }
 }
 
 export default new AddRecipeView();
