1d5f3e4561365e7f00b500880adc88a6d880bbcf
Lee Kah Keong <raintreeleaf1@gmail.com>
2025-04-17 14:49:48 +0800

    Changes to api url in config file

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const API_URL = `https://forkify-api.jonas.io/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 5;
-export const API_KEY = '40da0e8d-b201-4561-9665-6963776660b4';
+export const API_KEY = '68ca9515-7181-4f1c-a9e7-13c0f4ede54c';
 export const MODAL_CLOSE_SEC = 2.5;
